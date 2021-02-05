89dce43c00ed572cb096bd8352b694ad92f33497
M Nazir <monazir1001@gmail.com>
2021-02-05 00:25:21 +0000

    header files for separating structs out when getting user input to calculate subnet range

---
+++ src/headers/IpClasses.h
@@ -0,0 +1,8 @@
+#ifndef IP_CLASSES_H
+#define IP_CLASSES_H
+
+struct IpClasses {
+    char ipClassChoice;
+} ipClass;
+
+#endif // !IP_CLASSES_H
---
+++ src/headers/IpSubnet.h
@@ -0,0 +1,11 @@
+#include <string>
+
+#ifndef IP_SUBNET_H
+#define IP_SUBNET_H
+
+struct IpSubnet {
+    std::string address;
+    int cidr;
+} ipSubnet;
+
+#endif // !IP_SUBNET_H
